964d79805dde4584a7e945bde6a38169b1ff698c
Isac <isac_p_filho@hotmail.com>
2019-09-16 16:39:49 -0300

    uploading frontend and backend

---
+++ README.md
@@ -0,0 +1,5 @@
+# gitmeet-app
+This project was bootstrapped with Create React App.
+app made with node.js(backend) and react js(frontend)
+its a copy of tinder for developers, you can enter using your github username and match with others devs.
+i used github api, express, socket-io, axios and etc...
---
+++ backend/src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+
+const httpServer = express();
+const server = require('http').Server(httpServer);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+    connectedUsers[user] = socket.id;
+});
+
+mongoose.connect('mongodb+srv://isacsneto:isacsneto@cluster0-yqme3.mongodb.net/isacsneto8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+httpServer.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+httpServer.use(cors());
+httpServer.use(express.json());
+httpServer.use(routes);
+
+server.listen(8140);
